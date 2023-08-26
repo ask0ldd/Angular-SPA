804dbbd234aded02f97d3d1358e0722897e8d888
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-26 17:19:45 +0200

    rental inteface

--- src/app/models/rental.ts
+++ src/app/models/rental.ts
@@ -1,14 +1,17 @@
-export class Rental {
-    id : string;
-    title : string;
-    cover : string;
-    description : string;
-    host : {
-        name: string;
-        picture : string;
-    };
-    rating : number;
-    location : string;
-    equipments : Array<string>;
-    tags : Array<string>;
+export interface Rental {
+    id : string
+    title : string
+    cover : string
+    pictures : Array<string>
+    description : string
+    host : Host
+    rating : string
+    location : string
+    equipments : Array<string>
+    tags : Array<string>
+}
+
+interface Host{
+    name : string
+    picture : string
 }
