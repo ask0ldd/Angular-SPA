b2308c4f7ea2bfd2d0a372044b236e5894780f89
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-26 05:53:07 +0200

    some commentaries added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root', // tag name of this custom component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularprj';
+
+  // constructor method is not defined for most angular component
+
+  ngOnInit(): void {
+    console.log(this.title, "aaa")
+  }
 }
 
-// console.log(new AppComponent())
+// https://angular.io/guide/lifecycle-hooks
