2098804b1e6bdb90ed2c164460e54a8b6a40d6d8
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-27 19:17:33 +0200

    pipe cleaner integration

--- src/app/rating-color.pipe.ts
+++ src/app/rating-color.pipe.ts
@@ -7,8 +7,11 @@ export class RatingColorPipe implements PipeTransform {
 
   // transform(value: unknown, ...args: unknown[]): unknown {
   transform(rating : string) : string {
-    if(parseInt(rating) > 4) return " [Recommanded]"
-    return ""
+    /*if(parseInt(rating) > 4) return " [Recommanded]" // can be used to control the css properties of a tag, like the color of a pill changing with the innertag value
+    return ""*/
+    if(parseInt(rating) > 4) return "green"
+    if(parseInt(rating) > 3) return "yellow"
+    return "orange"
   }
 
 }
