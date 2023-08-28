ef68a35572ede70913e373685cc78846307a4aef
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-28 06:34:39 +0200

    banner

--- src/app/rentals/rentals.module.ts
+++ src/app/rentals/rentals.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { immocardHoverDirective } from './immocard-hover.directive';
-import { Page404Component } from '../page404/page404.component';
 import { RatingColorPipe } from './rating-color.pipe';
 import { RentalDetailsComponent } from './rental-details/rental-details.component';
 import { RentalsGalleryComponent } from './rentals-gallery/rentals-gallery.component';
