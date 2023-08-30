16e59f0472eeb61de5b7a19bcbc93316ee207be6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-30 08:32:44 +0200

    template ref clearing when newtag / neweq push successful

--- src/app/rentals/rentals-edit/rentals-edit.component.ts
+++ src/app/rentals/rentals-edit/rentals-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RentalsService } from '../rentals.service';
 import { Rental } from 'src/app/models/rental';
@@ -13,6 +13,12 @@ export class RentalsEditComponent {
   rentalId : string | null
   editedRental : Rental | undefined
 
+  @ViewChild('newTag'/*, { static: true }*/) 
+  newTag: ElementRef
+
+  @ViewChild('newEquipment') 
+  newEquipment: ElementRef
+
   constructor(private router:Router, private route: ActivatedRoute, private rentalService : RentalsService){ }
 
   ngOnInit(): void {
@@ -24,6 +30,8 @@ export class RentalsEditComponent {
     }
     this.editedRental = this.rentalService.getRentalById(this.rentalId)
   }
+
+  ngAfterViewInit() {}
 
   onSubmit() : void {
     if(this.rentalId != null) this.router.navigateByUrl('/rental/'+this.rentalId) 
@@ -39,11 +47,17 @@ export class RentalsEditComponent {
 
   addEquipment(equipmentValue : string){
     // should verify if value is valid
-    if (this.rentalId!=null) this.rentalService.addEquipment(equipmentValue, this.rentalId)
+    if (this.rentalId!=null) {
+      const isSuccessful = this.rentalService.addEquipment(equipmentValue, this.rentalId)
+      if(isSuccessful) this.newEquipment.nativeElement.value=""
+    }
   }
 
   addTag(equipmentValue : string){
     // should verify if value is valid
-    if (this.rentalId!=null) this.rentalService.addTag(equipmentValue, this.rentalId)
+    if (this.rentalId!=null) {
+      const isSuccessful = this.rentalService.addTag(equipmentValue, this.rentalId)
+      if(isSuccessful) this.newTag.nativeElement.value=""
+    }
   }
 }
