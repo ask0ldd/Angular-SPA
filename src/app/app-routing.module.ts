78b4cb734b05095789ad49c3cf70e26b539a5748
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-31 22:57:21 +0200

    base login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Page404Component } from './page404/page404.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   /*{path: 'gallery', component: RentalsGalleryComponent},
   {path: 'rental/:id', component: RentalDetailsComponent},*/
+  {path: 'login', component: LoginComponent},
   {path: '', redirectTo: 'gallery', pathMatch: 'full'}, // page by default
   {path: '**', component: Page404Component}, // non existing url
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { Page404Component } from './page404/page404.component';
 import { RentalsModule } from './rentals/rentals.module';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [ // components, directives, pipes to import
     AppComponent, 
-    Page404Component,
+    Page404Component, LoginComponent,
     // immocardHoverDirective, RatingColorPipe, RentalsGalleryComponent, RentalDetailsComponent, Page404Component
   ],
   imports: [ // other modules to import
