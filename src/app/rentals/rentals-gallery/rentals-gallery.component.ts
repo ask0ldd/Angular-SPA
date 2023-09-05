03dfcf22954e0c4ec3b5ba3a9a326d9b84196f28
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-05 05:15:43 +0200

    gallery component clean up

--- src/app/rentals/rentals-gallery/rentals-gallery.component.ts
+++ src/app/rentals/rentals-gallery/rentals-gallery.component.ts
@@ -18,7 +18,7 @@ export class RentalsGalleryComponent implements OnInit {
   /*rentalsList : Array<Rental>
   APIRentalsList : Array<IRental>*/
   selectedRental : Rental | undefined
-  likedRentals : Array<string> = []
+  likedRentals : Array<string | number> = []
 
   rentals : Array< Rental | IRental>
 
@@ -27,11 +27,7 @@ export class RentalsGalleryComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.APIAsSource = true
-    if(this.APIAsSource) {
-      this.rentals = await this.apiService.getAllRentals()
-    }else{
-      this.rentals = this.rentalService.getAllRentals()
-    }
+    this.rentals = this.APIAsSource ? await this.apiService.getAllRentals() : this.rentalService.getAllRentals()
   }
 
   /* kept as a learning reference
@@ -75,7 +71,7 @@ export class RentalsGalleryComponent implements OnInit {
     }
   }
 
-  switchRentalLikeStatus(e: MouseEvent, rentalId : string) : any {
+  switchRentalLikeStatus(e: MouseEvent, rentalId : string | number) : any {
     e.preventDefault()
     e.stopPropagation()
     if(this.likedRentals.includes(rentalId) === false || this.likedRentals === undefined) return this.likedRentals.push(rentalId)
@@ -83,7 +79,7 @@ export class RentalsGalleryComponent implements OnInit {
     this.likedRentals.splice(indexOfRentalToUnlike, 1)
   }
 
-  isRentalLiked(rentalId : string) : string {
+  isRentalLiked(rentalId : string | number) : string {
     return this.likedRentals === undefined || this.likedRentals.includes(rentalId) === false ? "assets/favoutline.svg" : "assets/favfull.svg"
   }
 }
