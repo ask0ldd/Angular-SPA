97348788ff5a3c1e27d998d43bd570e029ea06ca
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-10 21:11:38 +0200

    cookies methods

--- src/app/auth/cookies.service.ts
+++ src/app/auth/cookies.service.ts
@@ -7,11 +7,18 @@ export class CookiesService {
 
   constructor() { }
 
-  setCookie(cookieDatas : object){
-
+  setCookie(cookieDatas : {userId : string, token : string}){
+    document.cookie = `id=${cookieDatas.userId}; Secure`
+    document.cookie = `token=${cookieDatas.token}; Secure`
   }
 
   eraseCookie(){
-    
+    document.cookie = "token=; Max-Age=0;"
+    document.cookie = "id=; Max-Age=0;"
+  }
+
+  isTokenAlive(){
+    const cookie = document.cookie
+    return cookie.search("token")===-1 ? false : true
   }
 }
